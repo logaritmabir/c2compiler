c309a32a50900e9d5873deab1b11e41271d2585c
logaritmabir <emre.ozer@bil.omu.edu.tr>
2024-10-11 02:14:41 +0300

    init files

---
+++ demo.c
@@ -0,0 +1,28 @@
++90 555 789 43 69
++905557894369
+
+asdasd@gmail.com
+asdsa33@gmail.com
+asdsa33.mrb@gmail.com
+asdsa33.mrb@gmail.com.tr
+asdsa33.mrb@hexmail.com
+asdsa33.mrb@hexmail.com.uk
+asdsa33.-mrb@hexmail.com.uk
+
+#include <stdio.h>
+
+int main(void){
+    for(int i = 0; i < 10; i++){
+        printf("bla bla %d\n", i);
+    }
+
+    int x = 0;
+
+    while(0){}
+
+    int y = x + 3;
+}
+
+int add(int x , int y){
+    return x+y;
+}
